e1ebea43a81225460d8175c1e16eae7acbd09b36
AntonioDaSilva <mertunsal1905@gmail.com>
2022-08-17 17:01:10 +0300

    working setup for the package

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from gettext import install
-from setuptools import setup
+import setuptools
 
 install_requires = ['gym>=0.25.1','numpy>=1.23.1', 'matplotlib>=3.5.1', 'networkx>=2.8', 'gurobipy>=9.5.2', 'methodtools>=0.4.5']
 
-setup(name='ubergym',
+setuptools.setup(name='ubergym',
     version='0.0.1',
     description='Open AI gym interface to simulate Uber environment with driver agents',
     author = 'Mert Unsal',
     author_email = 'mailmertunsal@gmail.com',
     python_requires='>=3.9',
     install_requires=install_requires,
+    packages=setuptools.find_packages(),
     include_package_data=True,
 )
